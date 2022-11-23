6d701d3aa6433d7b94f51242256c29142e0322c2
unknown <Administrador@HOWS-DSAN-0004.motiva.matriz>
2022-11-22 21:05:37 -0300

    Add Base projeto .Net

---
+++ MVCprojeto/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using MVCprojeto.Models;
+using MVCprojeto.Validator;
+
+namespace MVCprojeto.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserViewModel user = new UserViewModel();
+            return View("Index", user);
+            
+        }
+        [HttpPost]
+        public IActionResult teste(UserViewModel user)
+        {
+            UserValidator validator = new UserValidator();
+            ValidationResult result = validator.Validate(user);
+
+
+            if (!result.IsValid)
+            {
+                foreach (var failure in result.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+
+            }
+            
+            user.Email = "email enviado";
+
+            return View("Index",user);
+        }
+    }
+}
---
+++ MVCprojeto/Validator/UserValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MVCprojeto.Models;
+
+namespace MVCprojeto.Validator
+{
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+        public UserValidator() {
+            RuleFor(user => user.Email).NotNull().WithMessage("Favor preencha o e-mail  ");
+            RuleFor(user => user.Email).EmailAddress().WithMessage("Formato do e-mail é invalido  ");
+            RuleFor(user => user.Password).NotNull().WithMessage("Favor digite a senha  ");
+        }
+    }
+}
